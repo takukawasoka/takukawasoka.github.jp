b243f66fbc38ceb7a1caa92cc1789f27cf7fbb3c
takukawasoka <68958321+takukawasoka@users.noreply.github.com>
2020-07-30 00:15:05 +0900

    Add files via upload

---
+++ js/d.js
@@ -0,0 +1,12 @@
+function hand(judge){
+    var message = "";
+    
+    if (judge == 1){
+        message = "残念。正解は3番の100mでした。思ったよりも高いです！";
+    }else if(judge == 2){
+        message = "残念。正解は3番の100mでした。思ったよりも高いです！";
+    }else if(judge == 3){
+        message = "正解！大阪旅行に行ってみよう！";
+    }
+    alert(message);
+}
